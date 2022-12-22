18b3f9ac016775016bc2fe4c795611e0a8189ae7
Hemant Parihar <hepariha@cisco.com>
2022-12-22 15:11:24 +0530

    Configure the project with the required dependencies.

---
+++ src/main/java/com/kafka/learning/SpringbootKafkaTutsApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutsApplication.class, args);
+	}
+
+}
